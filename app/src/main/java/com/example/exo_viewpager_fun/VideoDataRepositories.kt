53ee60337626f94e9a751c2c91ad68eb5afa2932
Nick Rose <nihk@github.com>
2021-10-17 21:28:17 -0400

    Add remote video data repository

--- app/src/main/java/com/example/exo_viewpager_fun/VideoDataRepositories.kt
+++ app/src/main/java/com/example/exo_viewpager_fun/VideoDataRepositories.kt
@@ -9,6 +9,19 @@ interface VideoDataRepository {
     fun videoData(): Flow<List<VideoData>>
 }
 
+// Remote videos that worked at the time of this commit, but might not by the time you try them!
+// Note that the video previews are local assets. In a real app, they should be served by your backend.
+class RemoteVideoDataRepository : VideoDataRepository {
+    override fun videoData(): Flow<List<VideoData>> {
+        val videoData = listOf(
+            AssetVideoData.waves.copy(mediaUri = "https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4"),
+            AssetVideoData.christmas.copy(mediaUri = "https://assets.mixkit.co/videos/preview/mixkit-the-spheres-of-a-christmas-tree-2720-large.mp4"),
+            AssetVideoData.yellow.copy(mediaUri = "https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4")
+        )
+        return flowOf(videoData)
+    }
+}
+
 // Here for demo purposes I've used assets local to the app. This could be any other implementation,
 // however. e.g. remotely fetched videos via Retrofit, Firebase, etc.
 class OneShotAssetVideoDataRepository : VideoDataRepository {
