1b538f768f7fde544ed407137559364d19e7952d
Nick Rose <nickjrose@gmail.com>
2021-11-22 19:19:44 -0500

    Don't use sealed interfaces needlessly

--- app/src/main/java/com/example/exo_viewpager_fun/models/ViewEffects.kt
+++ app/src/main/java/com/example/exo_viewpager_fun/models/ViewEffects.kt
@@ -2,7 +2,7 @@ package com.example.exo_viewpager_fun.models
 
 import androidx.annotation.DrawableRes
 
-sealed interface ViewEffect
-sealed class PlayerViewEffect : ViewEffect
+sealed class ViewEffect
+sealed class PlayerViewEffect : ViewEffect()
 data class AnimationEffect(@DrawableRes val drawable: Int) : PlayerViewEffect()
 object ResetAnimationsEffect : PlayerViewEffect()
--- app/src/main/java/com/example/exo_viewpager_fun/models/ViewEvents.kt
+++ app/src/main/java/com/example/exo_viewpager_fun/models/ViewEvents.kt
@@ -1,10 +1,10 @@
 package com.example.exo_viewpager_fun.models
 
-sealed interface ViewEvent
-object LoadVideoDataEvent : ViewEvent
-sealed class PlayerLifecycleEvent : ViewEvent {
+sealed class ViewEvent
+object LoadVideoDataEvent : ViewEvent()
+sealed class PlayerLifecycleEvent : ViewEvent() {
     object Start : PlayerLifecycleEvent()
     data class Stop(val isChangingConfigurations: Boolean) : PlayerLifecycleEvent()
 }
-object TappedPlayerEvent : ViewEvent
-data class OnPageSettledEvent(val page: Int) : ViewEvent
+object TappedPlayerEvent : ViewEvent()
+data class OnPageSettledEvent(val page: Int) : ViewEvent()
