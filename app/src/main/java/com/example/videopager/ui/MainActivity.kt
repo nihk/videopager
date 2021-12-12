b48d8e67c5516035b98593ca30f2e836486ec47e
Nick Rose <nickjrose@gmail.com>
2021-12-11 19:00:38 -0500

    Use view binding in main activity

--- app/src/main/java/com/example/videopager/ui/MainActivity.kt
+++ app/src/main/java/com/example/videopager/ui/MainActivity.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
-import com.example.videopager.R
+import com.example.videopager.databinding.MainActivityBinding
 import com.example.videopager.di.MainModule
 
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Manual dependency injection
         val module = MainModule(this)
         supportFragmentManager.fragmentFactory = module.fragmentFactory
+
         super.onCreate(savedInstanceState)
+        val binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
-                replace<MainFragment>(R.id.fragment_container)
+                replace<MainFragment>(binding.fragmentContainer.id)
             }
         }
     }
