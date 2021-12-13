404121a962903004b7097d78fd015d9fc56122a1
Nick Rose <nickjrose@gmail.com>
2021-12-12 21:01:00 -0500

    Drop initial onPageSelected emission

--- app/src/main/java/com/example/videopager/ui/extensions/ViewPager2Ext.kt
+++ app/src/main/java/com/example/videopager/ui/extensions/ViewPager2Ext.kt
@@ -4,6 +4,7 @@ import androidx.viewpager2.widget.ViewPager2
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.drop
 
 fun ViewPager2.pageIdlings(): Flow<Unit> = callbackFlow {
     val callback = object : ViewPager2.OnPageChangeCallback() {
@@ -28,6 +29,6 @@ fun ViewPager2.pageChanges(): Flow<Unit> = callbackFlow {
     registerOnPageChangeCallback(callback)
 
     awaitClose { unregisterOnPageChangeCallback(callback) }
-}
+}.drop(1) // Ignore the initial emission which happens immediately on subscription
 
 val ViewPager2.isIdle get() = scrollState == ViewPager2.SCROLL_STATE_IDLE
